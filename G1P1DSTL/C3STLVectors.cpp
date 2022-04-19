d90553dd0bbd5e153f661b141f1cfb888303a8fc
ersaitej <er.saitej@gmail.com>
2022-04-20 00:36:19 +0530

    Vectors implemented

--- G1P1DSTL/C3STLVectors.cpp
+++ G1P1DSTL/C3STLVectors.cpp
@@ -1,15 +1,65 @@
 /*
     Author: Sai Tej Sunkara
-    Date: 
-    Topic: 
+    Date: 20th April 2022
+    Topic: Vectors
 */
 #include<bits/stdc++.h>
+#include<vector>
 #include<algorithm>
 #define int long long int
 #define endl "\n"
 using namespace std;
 
+void sizeAndCapacity(vector<int> &vec) {
+    cout<<"Size of the vector is "<<vec.size()<<" and capacity is "<<vec.capacity()<<endl;
+}
+
+void displayVector(vector<int> &vec) {
+    cout<<"Displaying vector"<<endl;
+    for(vector<int>::iterator itr=vec.begin(); itr!=vec.end(); itr++) {
+        cout<<*itr<<" ";
+    }
+    cout<<endl;
+}
+
+void clearVector(vector<int> &vec) {
+    cout<<"Clearing vector"<<endl;
+    vec.clear();
+    displayVector(vec);
+}
+
+void insertElements(vector<int> &vec) {
+    int num;
+    for(int i=0; i<5; i++) {
+        cin>>num;
+        cout<<num<<" is inserted and ";
+        vec.push_back(num);
+        sizeAndCapacity(vec);
+    }
+    displayVector(vec);
+}
+
+void popElement(vector<int> &vec) {
+    cout<<vec.back()<<" is last element of vector"<<endl;
+    vec.pop_back();
+    displayVector(vec);
+}
+
+void popAllElements(vector<int> &vec) {
+    while(!vec.empty()) {
+        cout<<vec.back()<<" is last element of vector"<<endl;
+        vec.pop_back();
+    }
+    displayVector(vec);
+}
+
 int solve(int num) {
+    vector<int> vec;
+    insertElements(vec);
+    clearVector(vec);
+    insertElements(vec);
+    popElement(vec);
+    popAllElements(vec);
     return num;
 }
 
